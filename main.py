d7e17a9f778d8f84f5376d041e741b434109e2de
Anna Dvorkin <anna.dvorkin@teampicnic.com>
2025-07-04 19:52:48 +0000

    user custom RAG prompt

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_text_splitters import CharacterTextSplitter
 
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == "__main__":
 
@@ -44,3 +47,26 @@ if __name__ == "__main__":
     result = retrieval_chain.invoke(input={"input": query})
 
     print(result)
+
+    template = """Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    User three sentences maximum and keep the anseer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer.
+
+    {context}
+
+    Question: {question}
+
+    Helpful Answer:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": vectore_store.as_retriever() | format_docs,
+          "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res)
